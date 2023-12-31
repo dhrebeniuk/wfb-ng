9ae9131155161dddeb5d77a91dc27a2cf8dcaf76
dhrebeniuk <dmytrohrebeniuk@gmail.com>
2023-12-31 21:09:31 +0200

    Update tx.cpp

--- src/tx.cpp
+++ src/tx.cpp
@@ -179,10 +179,10 @@ void PcapTransmitter::inject_packet(const uint8_t *buf, size_t size)
     memcpy(p, buf, size);
     p += size;
 
-    const char *lastError = pcap_geterr(ppcap[current_output]);
-    if (lastError != nullptr && strlen(lastError) > 0) {
-        throw runtime_error(string_format(pcap_geterr(ppcap[current_output])));
-    }
+    // const char *lastError = pcap_geterr(ppcap[current_output]);
+    // if (lastError != nullptr && strlen(lastError) > 0) {
+    //     throw runtime_error(string_format(pcap_geterr(ppcap[current_output])));
+    // }
     
     if (current_output >= 0)
     {
