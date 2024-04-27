1c9e4b977c55621c38df408f22e1b8fea6130272
dhrebeniuk <dmytrohrebeniuk@gmail.com>
2024-04-27 11:45:51 +0300

    Update radiotap.c

--- src/radiotap.c
+++ src/radiotap.c
@@ -59,11 +59,15 @@ static const struct radiotap_align_size rtap_namespace_sizes[] = {
 	 */
 };
 
-
 static const struct ieee80211_radiotap_namespace radiotap_ns = {
-	.n_bits = ARRAY_SIZE(rtap_namespace_sizes),
-	.align_size = rtap_namespace_sizes,
+    .align_size = rtap_namespace_sizes,
+    .n_bits = sizeof(rtap_namespace_sizes) / sizeof(rtap_namespace_sizes[0]),
 };
+
+// static const struct ieee80211_radiotap_namespace radiotap_ns = {
+// 	.n_bits = ARRAY_SIZE(rtap_namespace_sizes),
+// 	.align_size = rtap_namespace_sizes,
+// };
 
 /**
  * ieee80211_radiotap_iterator_init - radiotap parser iterator initialization
